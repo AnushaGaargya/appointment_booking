7b2454b9b447d5dd1b24ffd376e68e7d851f1ac8
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-15 23:48:34 +0530

    added option to delete appointment data from crudcrud and from the UI

--- index.js
+++ index.js
@@ -45,22 +45,40 @@ function showNewUserOnScreen(user){
     var email = user.email;
     var phone = user.phone;
     var time = user.time;
+    var id = user._id;
 
     var paragraph = document.createElement("P");
       paragraph.style.color = "black";
-      paragraph.innerHTML = `${name}-${email}-${phone}-${time}`;
+      paragraph.innerHTML = `${id}-${name}-${email}-${phone}-${time}`;
       document.getElementById("content").appendChild(paragraph);
        // create delete button
     var deleteBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
     deleteBtn.appendChild(document.createTextNode('Delete'));
     paragraph.appendChild(deleteBtn);
-    // deleteBtn.addEventListener("click", delete_fn);
+    deleteBtn.addEventListener("click", delete_fn);
         // create edit button
     var editBtn = document.createElement('button');
     editBtn.className = 'btn btn-warning btn-sm float-right edit';
     editBtn.appendChild(document.createTextNode('Edit'));
     paragraph.appendChild(editBtn);
     // editBtn.addEventListener("click", edit_fn);
+}
+
+function delete_fn(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            var item = e.target.parentElement; //the parent element of delete btn is list item. 
+            document.getElementById("content").removeChild(item);
+            const first = (item.textContent).split('-')[0]
+            axios.delete(`https://crudcrud.com/api/9476a29dbbaa4177ba2c05387a3cc305/appointmentdata/${first}`)
+            .then((response) => {
+            console.log(response)})
+            .catch((error) => {
+                console.log(error) 
+                })
+        
+        }
+    }
 }
 
