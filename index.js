b0d7313d32fa35426dfac066e2fb6b1a7496830f
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-15 23:13:21 +0530

    included GET to retrieve data from crudcrud

--- index.js
+++ index.js
@@ -27,3 +27,40 @@ axios.post('https://crudcrud.com/api/9476a29dbbaa4177ba2c05387a3cc305/appointmen
 
 };
 
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/9476a29dbbaa4177ba2c05387a3cc305/appointmentdata")
+    .then((response) => {
+    console.log(response)
+    for(var i=0; i<response.data.length; i++){
+        showNewUserOnScreen(response.data[i])
+    }
+            })
+    .catch((error) => {
+    console.log(error) 
+    })
+})
+
+function showNewUserOnScreen(user){
+    var name = user.name;
+    var email = user.email;
+    var phone = user.phone;
+    var time = user.time;
+
+    var paragraph = document.createElement("P");
+      paragraph.style.color = "black";
+      paragraph.innerHTML = `${name}-${email}-${phone}-${time}`;
+      document.getElementById("content").appendChild(paragraph);
+       // create delete button
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    paragraph.appendChild(deleteBtn);
+    // deleteBtn.addEventListener("click", delete_fn);
+        // create edit button
+    var editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-warning btn-sm float-right edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    paragraph.appendChild(editBtn);
+    // editBtn.addEventListener("click", edit_fn);
+}
+
